031c8fed7633796e6cf2e58abab98c0cbe9d1a10
井24 <1933310737@qq.com>
2023-09-07 14:18:03 +0800

    Initial Commit

---
+++ PickDinnerIOS/PickDinnerIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickDinnerIOSApp.swift
+//  PickDinnerIOS
+//
+//  Created by 小昊 on 2023/9/7.
+//
+
+import SwiftUI
+
+@main
+struct PickDinnerIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
